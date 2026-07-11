562b4dd129439f533685c410aeda5db55f05069e
marthandan77 <marthandan@gmail.com>
2026-07-11 19:37:54 +0800

    Export probability calibration contracts

--- src/ung_forecast/models/__init__.py
+++ src/ung_forecast/models/__init__.py
@@ -1,6 +1,13 @@
-"""Probability models and versioned horizon registry."""
+"""Probability models, calibration, and versioned horizon registry."""
 
+from .calibration import CalibrationConfig, MulticlassProbabilityCalibrator
 from .elastic_net import ElasticNetMultinomialModel
 from .registry import HorizonModelRegistry, ModelRecord
 
-__all__ = ["ElasticNetMultinomialModel", "HorizonModelRegistry", "ModelRecord"]
+__all__ = [
+    "CalibrationConfig",
+    "ElasticNetMultinomialModel",
+    "HorizonModelRegistry",
+    "ModelRecord",
+    "MulticlassProbabilityCalibrator",
+]
