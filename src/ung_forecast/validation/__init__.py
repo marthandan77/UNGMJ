e4d5a6569bff116b7b93827d15cb1651aa18a7d9
marthandan77 <marthandan@gmail.com>
2026-07-11 18:47:42 +0800

    Create validation package

---
+++ src/ung_forecast/validation/__init__.py
@@ -0,0 +1,5 @@
+"""Purged walk-forward validation utilities."""
+
+from .walk_forward import WalkForwardFold, generate_walk_forward_folds
+
+__all__ = ["WalkForwardFold", "generate_walk_forward_folds"]
