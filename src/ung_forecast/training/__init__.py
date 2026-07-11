cb8a24f0e157b8d91f97b7ce9c6844e2fee7dd76
marthandan77 <marthandan@gmail.com>
2026-07-11 21:44:03 +0800

    Export empirical and session training workflows

--- src/ung_forecast/training/__init__.py
+++ src/ung_forecast/training/__init__.py
@@ -1,5 +1,14 @@
-"""Dataset construction and model-training utilities."""
+"""Dataset construction and empirical model-training utilities."""
 
 from .dataset import TrainingDataset, build_training_dataset
+from .empirical import EmpiricalRunConfig, EmpiricalRunResult, run_empirical_evaluation
+from .session_dataset import build_session_training_dataset
 
-__all__ = ["TrainingDataset", "build_training_dataset"]
+__all__ = [
+    "EmpiricalRunConfig",
+    "EmpiricalRunResult",
+    "TrainingDataset",
+    "build_session_training_dataset",
+    "build_training_dataset",
+    "run_empirical_evaluation",
+]
