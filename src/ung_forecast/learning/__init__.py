fe6e9a2cb8d8cafbd79a3622c729d21f605c6346
marthandan77 <marthandan@gmail.com>
2026-07-11 20:37:55 +0800

    Create champion challenger package

---
+++ src/ung_forecast/learning/__init__.py
@@ -0,0 +1,13 @@
+"""Controlled champion-challenger learning workflows."""
+
+from .champion_challenger import (
+    CandidateEvaluation,
+    ChampionChallengerManager,
+    PromotionDecision,
+)
+
+__all__ = [
+    "CandidateEvaluation",
+    "ChampionChallengerManager",
+    "PromotionDecision",
+]
