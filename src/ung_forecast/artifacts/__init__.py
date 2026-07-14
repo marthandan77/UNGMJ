c2a95672f0275b72fb35beaf1cabf8e9e4d67216
marthandan77 <marthandan@gmail.com>
2026-07-14 12:46:05 +0800

    Keep artifact public API stable

--- src/ung_forecast/artifacts/__init__.py
+++ src/ung_forecast/artifacts/__init__.py
@@ -7,7 +7,6 @@ from .loader import (
     load_manifest,
 )
 from .manifest import ArtifactFile, HorizonArtifactManifest, StatisticalMetricsSnapshot
-from .materialize import materialize_repository_artifacts
 from .runtime import LoadedHorizonArtifacts, load_validated_artifacts
 
 __all__ = [
@@ -20,5 +19,4 @@ __all__ = [
     "discover_horizon_artifacts",
     "load_manifest",
     "load_validated_artifacts",
-    "materialize_repository_artifacts",
 ]
